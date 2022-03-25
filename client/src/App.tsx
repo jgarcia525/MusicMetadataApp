239b864d6e1660bb6f36669f9d8f598fa3472b06
Jorge Garcia <jorgegarcia525@gmail.com>
2022-03-25 18:59:34 -0500

    add react setup

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <div>Hello world!</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
